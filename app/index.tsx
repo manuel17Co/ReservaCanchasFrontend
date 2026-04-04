cd79a9f688099d47ed06563fcd8273734675178c
Manuel M Rubio <manuelmunozrubio069@gmail.com>
2026-04-04 14:23:05 -0500

    Conexion con el backend e implementacion del register (Exitoso y redireccion al login)

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,11 @@
+import FormularioRegisterComponent from "@/app/(auth)/FormularioRegisterComponent";
 import { Text, View } from "react-native";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View style={{ flex: 1 }}>
+      <FormularioRegisterComponent />
     </View>
   );
 }
+
---
+++ src/services/ApiClient.ts
@@ -0,0 +1,73 @@
+import axios, { AxiosInstance, AxiosRequestConfig } from 'axios';
+// import { secureStoreService } from './SecureStoreService'; 
+
+const BASE_URL = process.env.EXPO_PUBLIC_API_RESERVAS;
+const TOKEN_KEY = "auth_token";
+
+class ApiClient {
+    private client: AxiosInstance;
+    
+    constructor() {
+        this.client = axios.create({
+            baseURL: BASE_URL,
+            headers: {
+                'Content-Type': 'application/json',
+            }
+        });
+    }
+
+    private async buildHeaders(useToken: boolean): Promise<AxiosRequestConfig["headers"]> {
+        if (!useToken) return {};
+        
+        // const token = await secureStoreService.get<string>(TOKEN_KEY);
+        // return token ? { Authorization: `Bearer ${token}` } : {};
+        return {};
+    }
+
+    async get<T>(endpoint: string, useToken: boolean = false, config?: AxiosRequestConfig): Promise<T> {
+        const headers = await this.buildHeaders(useToken);
+        const res = await this.client.get<T>(endpoint, { 
+            ...config, 
+            headers: { ...headers, ...config?.headers } 
+        });
+        return res.data;
+    }
+
+    async post<T, K>(endpoint: string, body: K, useToken: boolean = false, config?: AxiosRequestConfig): Promise<T> {
+        const headers = await this.buildHeaders(useToken);
+        const res = await this.client.post<T>(endpoint, body, {
+            ...config,
+            headers: { ...headers, ...config?.headers }
+        });
+        return res.data;
+    }
+
+    async put<T, K>(endpoint: string, body: K, useToken: boolean = false, config?: AxiosRequestConfig): Promise<T> {
+        const headers = await this.buildHeaders(useToken);
+        const res = await this.client.put<T>(endpoint, body, {
+            ...config,
+            headers: { ...headers, ...config?.headers }
+        });
+        return res.data;
+    }
+
+    async patch<T, K>(endpoint: string, body: K, useToken: boolean = false, config?: AxiosRequestConfig): Promise<T> {
+        const headers = await this.buildHeaders(useToken);
+        const res = await this.client.patch<T>(endpoint, body, {
+            ...config,
+            headers: { ...headers, ...config?.headers }
+        });
+        return res.data;
+    }
+
+    async delete<T>(endpoint: string, useToken: boolean = false, config?: AxiosRequestConfig): Promise<T> {
+        const headers = await this.buildHeaders(useToken);
+        const res = await this.client.delete<T>(endpoint, {
+            ...config,
+            headers: { ...headers, ...config?.headers }
+        });
+        return res.data;
+    }
+}
+
+export const apiClient = new ApiClient();
